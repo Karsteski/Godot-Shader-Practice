a6227943faa1dfcf576e5930b8274fc7d754939b
Kareem Skinner <kareem@karsteski.com>
2024-12-27 15:28:47 -0500

    Added showcase for manipulating a sine line

---
+++ sprite_2d.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+# @onready var my_shader = preload("res://shader.gdshader")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	# var image = Image.create_empty(256, 256, false, Image.FORMAT_RGB8)
+	# texture = ImageTexture.create_from_image(image)
+	# texture.create_placeholder()
+	#
+	# material = ShaderMaterial.new()
+	# material.shader = my_shader
